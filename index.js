defb7ca99e4c8708428cd5fa264572095d4fc438
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-10-27 23:22:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+let x = 1138;
+let у = 3 / 8;
+let s = "3-8";
+x = у;
+let n = 3 - 'one';
+let t = "one" + "two";
+let 3po = true; // error
+let level_ = 11;
+let highNoon = false;
+let $ = 21.30;
+let z = 2000;
+let isBig = у > z;
+z = z + 1;
+z--;
+z y; // error
+x = z * t;
+while (highNoon) {
+    z--;
+}
